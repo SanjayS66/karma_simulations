2d337a8c734db03e30be6cb44522c5871d6bedbb
SanjayS66 <sanjay702594@gmail.com>
2025-10-25 23:32:47 +0530

    ros2 control needs to be fixed

--- build/rcup_simulations/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/sanjay/ros2_workspaces/rcup_simul/src/rcup_simulations/install/rcup_simulations'
